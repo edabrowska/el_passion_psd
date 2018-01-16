6eb761ebdab10f035c565be79d08cdf3ca7c821f
Adam Borowski <adam@adamboro.com>
2018-01-16 22:52:19 +0000

    chore(webpack): add bundle analysis npm script

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const { ANALYZE } = process.env
+
 module.exports = {
   exportPathMap: function() {
     return {
       '/': { page: '/' },
       '/about': { page: '/about' },
     }
+  },
+  webpack: function (config) {
+    if (ANALYZE) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerPort: 8888,
+        openAnalyzer: true
+      }))
+    }
+
+    return config
   }
 }
