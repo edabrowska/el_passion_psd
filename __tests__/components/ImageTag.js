dc1b10705dfee00069667a4afa0d4922f9f5d6b2
Adam Borowski <adam@adamboro.com>
2018-01-17 16:12:03 +0000

    test: configure jest & enzyme; add tests for ImageTag as example

---
+++ __tests__/components/ImageTag.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import ImageTag from '~/ImageTag'
+
+describe('ImageTag', () => {
+  const imgPath = 'test.png'
+  const someProp = {value: 42}
+  const wrapper = shallow(
+    <ImageTag src={imgPath} {...someProp} />
+  )
+  it('renders an img tag', () => {
+    expect(wrapper.type()).toEqual('img')
+  })
+  it('sets src attribute', () => {
+    expect(wrapper.props().src).toMatch(imgPath)
+  })
+  it('passes props', () => {
+    expect(wrapper.props().value).toEqual(someProp.value)
+  })
+})
