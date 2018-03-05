7ac48df5730dd49c7aa0facaf4025d436c9fae05
Kruczek <piotr.kruczek@daftcode.pl>
2018-03-05 12:08:50 +0100

    ci: update cd config
    - remove super slow cache

--- deploy.config.js
+++ deploy.config.js
@@ -1,31 +1,23 @@
-const sharedConfig = {
+const defaultConfig = {
   user: 'pupper',
-  ref: 'origin/master',
-  repo: 'ssh://git@gitlab.int.daftup.com:daftup/frontend/spark.git',
-  path: '/home/pupper/spark',
+  repo: '<repo-url>',
+  path: '/home/pupper/<project-name>',
   ssh_options: ['StrictHostKeyChecking=no', 'ForwardAgent=yes'],
-  'post-deploy': 'npm i && npm run export',
+  post-deploy: 'npm i && npm run export'
 }
 
 module.exports = {
   deploy: {
     staging: {
-      host: [
-        {
-          'host': 'spark.int.daftup.com',
-          'port': '2200'
-        }
-      ],
-      ...sharedConfig,
+      ...defaultConfig,
+      ref: 'origin/master',
+      host: [{
+        host: 'stg.int.daftup.com',
+        port: '2200'
+      }]      
     },
     production: {
-      host: [
-        {
-          'host': 'spark.int.daftup.com',
-          'port': '2200'
-        }
-      ],
-      ...sharedConfig,
+      ...defaultConfig
     }
   }
 }
