d859d136d437d29759b5783949a22c0638bfe3bb
Oleg Ruzhytskyi <oleg.ruzhytskyi@daftcode.pl>
2019-01-24 12:20:08 +0100

    feat(FileLoader): added pdf & webm format to file-loader

--- next.config.js
+++ next.config.js
@@ -56,7 +56,7 @@ const nextConfig = {
 
     config.module.rules.push(
       {
-        test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4)$/,
+        test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm)$/,
         use: [
           {
             loader: 'file-loader',
